eed876ea393741e0968f596b642dffc1c1e1b881
ylgon <111996771+ylgon@users.noreply.github.com>
2022-10-11 14:01:34 +1100

    Update veg_lit_vis.js

--- veg_lit_vis.js
+++ veg_lit_vis.js
@@ -1,4 +1,7 @@
 var vg_1="olympics.vg.json";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     }).catch(console.error);
- 
+var vg_2="olympics2022.vg.json";
+vegaEmbed("#bubble_chart", vg_2).then(function(result) {
+    }).catch(console.error);
+     
